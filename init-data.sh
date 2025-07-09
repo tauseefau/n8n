161a7eff3faed3e7418d35b3fea617bf09725e52
Mohammad Tauseef <tauseefau@live.com>
2025-07-09 17:28:58 +1000

    stage changes

--- init-data.sh
+++ init-data.sh
@@ -11,14 +11,3 @@ EOSQL
 else
   echo "SETUP INFO: No Environment variables given for n8n user!"
 fi
-
-# Setup for NocoDB-specific database and user
-if [ -n "${NOCODB_USER:-}" ] && [ -n "${NOCODB_PASSWORD:-}" ] && [ -n "${NOCODB_DB:-}" ]; then
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE ${NOCODB_DB};
-    CREATE USER ${NOCODB_USER} WITH ENCRYPTED PASSWORD '${NOCODB_PASSWORD}';
-    GRANT ALL PRIVILEGES ON DATABASE ${NOCODB_DB} TO ${NOCODB_USER};
-EOSQL
-else
-  echo "SETUP INFO: No Environment variables given for NocoDB user!"
-fi
